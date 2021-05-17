a0ceb4a5a65f19969f0dc490c8f3e341e4a5fdac
Bruno Cunha <bruncun@icloud.com>
2021-05-17 15:06:06 -0400

    fix unnecessary import warning

--- frontend-test-react/src/components/todos/TodoList.js
+++ frontend-test-react/src/components/todos/TodoList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NewTodoForm } from './NewTodoForm';
 import { selectAllTodos } from '../../features/todos/todosSlice';
 import { Todo } from './Todo';
 
