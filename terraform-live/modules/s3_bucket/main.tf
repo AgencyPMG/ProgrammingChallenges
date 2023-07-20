8080d1b725d72785993a2acb2fd3723ac9c70016
Phil Dier <phil@pmg.com>
2023-07-20 17:39:47 -0500

    remove deprecated acl resource

--- terraform-live/modules/s3_bucket/main.tf
+++ terraform-live/modules/s3_bucket/main.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "this" {
   bucket = var.name
 }
 
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
